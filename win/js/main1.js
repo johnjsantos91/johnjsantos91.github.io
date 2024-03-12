30e8992235c9232e5fb0d95c5c0f3b40f2491017
johnjsantos91 <johnjsantos91@gmail.com>
2024-03-12 15:44:13 -0700

    win project

---
+++ win/js/main1.js
@@ -0,0 +1,50 @@
+document.querySelector(".home2").addEventListener("click", function(){
+    document.querySelector(".home3").style.visibility = "visible";
+
+})
+
+document.querySelector(".home3").addEventListener("click", function(){
+    document.querySelector("#image-7").style.visibility = "visible";
+
+})
+
+document.querySelector("#image-7").addEventListener("click", function(){
+    document.querySelector("#image-8").style.visibility = "visible";
+
+})
+
+document.querySelector("#image-8").addEventListener("click", function(){
+    document.querySelector("#image-13").style.visibility = "visible";
+
+})
+
+document.querySelector("#image-13").addEventListener("click", function(){
+    document.querySelector("#image-14").style.visibility = "visible";
+
+})
+document.querySelector("#image-14").addEventListener("click", function(){
+    document.querySelector("#image-15").style.visibility = "visible";
+
+})
+
+document.querySelector("#image-9").addEventListener("click", function(){
+    document.querySelector("#image-9").style.visibility = "hidden";
+
+})
+document.querySelector("#image-10").addEventListener("click", function(){
+    document.querySelector("#image-10").style.visibility = "hidden";
+
+})
+
+document.querySelector("#image-11").addEventListener("click", function(){
+    document.querySelector("#image-11").style.visibility = "hidden";
+
+})
+document.querySelector("#image-12").addEventListener("click", function(){
+    document.querySelector("#image-12").style.visibility = "hidden";
+
+})
+
+
+
+
